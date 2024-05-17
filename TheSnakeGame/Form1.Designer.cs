8c296a77dac500743a6171c582ba541b693d02a0
ensarhamzic <ensarhamzic01@gmail.com>
2024-05-17 21:05:08 +0200

    add food and snake growing

--- TheSnakeGame/Form1.Designer.cs
+++ TheSnakeGame/Form1.Designer.cs
@@ -36,6 +36,13 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.highScoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.timerText = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,7 +58,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 529);
+            this.button1.Location = new System.Drawing.Point(652, 532);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 33);
             this.button1.TabIndex = 1;
@@ -64,11 +71,78 @@
             this.timer1.Interval = 70;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 549);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score:";
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.scoreText.Location = new System.Drawing.Point(78, 547);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(23, 25);
+            this.scoreText.TabIndex = 3;
+            this.scoreText.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 529);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "High Score:";
+            // 
+            // highScoreText
+            // 
+            this.highScoreText.AutoSize = true;
+            this.highScoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.highScoreText.Location = new System.Drawing.Point(108, 525);
+            this.highScoreText.Name = "highScoreText";
+            this.highScoreText.Size = new System.Drawing.Size(23, 25);
+            this.highScoreText.TabIndex = 5;
+            this.highScoreText.Text = "0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 529);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Time:";
+            // 
+            // timerText
+            // 
+            this.timerText.AutoSize = true;
+            this.timerText.Location = new System.Drawing.Point(467, 549);
+            this.timerText.Name = "timerText";
+            this.timerText.Size = new System.Drawing.Size(21, 16);
+            this.timerText.TabIndex = 7;
+            this.timerText.Text = "0s";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 583);
+            this.Controls.Add(this.timerText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.highScoreText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -78,6 +152,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +161,13 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label highScoreText;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timerText;
     }
 }
 
--- TheSnakeGame/Form1.cs
+++ TheSnakeGame/Form1.cs
@@ -12,6 +12,11 @@ namespace TheSnakeGame
         private bool madeMove = false;
         private int width = 37;
         private int height = 27;
+        private Point food;
+        private int score = 0;
+        private int highScore = 0;
+        private bool isGameRunning = false;
+        private int time = 0;
 
         public Form1()
         {
@@ -23,12 +28,55 @@ namespace TheSnakeGame
         {
             foreach (Point p in snake.body)   
                 e.Graphics.FillRectangle(Brushes.Green, p.x * snake.size, p.y * snake.size, snake.size, snake.size);
+
+            if (!isGameRunning) return;
+
+            if (food == null)
+            {
+                Random random = new Random();
+                int randomNumber;
+                int randomNumber2;
+                while (true)
+                {
+                    randomNumber = random.Next(0, 36);
+                    randomNumber2 = random.Next(0, 26);
+
+                    bool isOnSnake = false;
+
+                    foreach (Point p in snake.body)
+                    {
+                        if (p.x == randomNumber && p.y == randomNumber2)
+                        {
+                            isOnSnake = true;
+                            break;
+                        }
+                    }
+
+                    if (!isOnSnake)
+                        break;
+                }
+                food = new Point(randomNumber, randomNumber2);
+            }
+
+            e.Graphics.FillEllipse(Brushes.DarkRed, food.x * snake.size, food.y * snake.size, snake.size, snake.size);
         }
 
         private void timer1_Tick(object sender, System.EventArgs e)
         {
-            Point point = null;
             Point snakeHead = snake.body[0];
+
+            if(food != null)
+            {
+                if (snakeHead.x == food.x && snakeHead.y == food.y)
+                {
+                    snake.body.Add(new Point(snake.body[snake.body.Count - 1].x, snake.body[snake.body.Count - 1].y));
+                    food = null;
+                    score++;
+                    scoreText.Text = score.ToString();
+                }
+            }
+           
+
             int newX = 0;
             int newY = 0;
             switch (snake.direction)
@@ -55,9 +103,38 @@ namespace TheSnakeGame
                     break;
             }
 
+            foreach (Point p in snake.body)
+            {
+                if (p.x == newX && p.y == newY)
+                {
+                    timer1.Stop();
+                    gameTimer.Stop();
+
+                    if (score > highScore)
+                    {
+                        highScore = score;
+                        highScoreText.Text = highScore.ToString();
+                    }
+
+                    score = 0;
+                    time = 0;
+                    madeMove = false;
+                    food = null;
+                    snake = new Snake();
+                    scoreText.Text = score.ToString();
+                    timerText.Text = time.ToString() + "s";
+                    isGameRunning = false;
+                    GetPictureBox().Invalidate();
+                    return;
+                }
+            }
+
             snake.body.Insert(0, new Point(newX, newY));
             snake.body.RemoveAt(snake.body.Count - 1);
             madeMove = true;
+
+          
+
             GetPictureBox().Invalidate();
         }
 
@@ -86,14 +163,18 @@ namespace TheSnakeGame
             return base.ProcessCmdKey(ref msg, keyData);
         }
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            if (isGameRunning) return;
             timer1.Start();
+            gameTimer.Start();
+            isGameRunning = true;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            time++;
+            timerText.Text = time.ToString() + "s";
         }
     }
 }
