57639bf59212abdc301fb8ef0a0475d8c9ec135b
ensarhamzic <ensarhamzic01@gmail.com>
2024-05-19 14:30:25 +0200

    add translation and midpoint algorithm

--- TheSnakeGame/Form1.Designer.cs
+++ TheSnakeGame/Form1.Designer.cs
@@ -68,7 +68,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 70;
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
